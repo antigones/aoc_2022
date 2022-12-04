18ab2dca089c669f69c7e5b8ca31d07a5d60ee0e
antigones <juna.salviati@gmail.com>
2022-12-04 18:59:52 +0100

    code refactor

--- day3/main.py
+++ day3/main.py
@@ -11,50 +11,37 @@ def calc_value_for_char(char):
 
 
 def part1():
-    file1 = open("input.txt", 'r')
-    lines = file1.readlines()
     s = 0
-    for line in lines:
-        line = line.strip()
-        l = len(line)
-        half_index = int(l/2)
-        first_half, last_half = line[:half_index], line[half_index:]
-        first_set = set(first_half)
-        last_set = set(last_half)
-        in_common = first_set.intersection(last_set)
-        char_in_common = in_common.pop()
-        value_for_in_common = calc_value_for_char(char_in_common)
-        s += value_for_in_common
+    with open("input.txt") as input_file:
+        for line in input_file:
+            line = line.strip()
+            half_index = len(line.strip())//2
+            first_half, last_half = line[:half_index], line[half_index:]
+            in_common = set(first_half).intersection(set(last_half))
+            char_in_common = in_common.pop()
+            value_for_in_common = calc_value_for_char(char_in_common)
+            s += value_for_in_common
     print(s)
 
 
 def part2():
     N_LINES_IN_BATCH = 3
     s = 0
-    with open("input.txt") as f:
+    with open("input.txt") as input_file:
         while True:
-            lines = islice(f, N_LINES_IN_BATCH)
-
+            lines = islice(input_file, N_LINES_IN_BATCH)
             l_lines = list(lines)
             if not l_lines:
                 break
-            first_line, second_line, third_line = l_lines[0], l_lines[1], l_lines[2]
-            print(first_line)
+            first_line, second_line, third_line = l_lines
             first_set, second_set, third_set = set(first_line.strip()), set(
                 second_line.strip()), set(third_line.strip())
             in_common = first_set.intersection(
                 second_set).intersection(third_set)
             char_in_common = in_common.pop()
             value_for_in_common = calc_value_for_char(char_in_common)
-            print(char_in_common)
-            print(value_for_in_common)
             s += value_for_in_common
     print(s)
 
 
-def main():
-    part2()
-
-
-if __name__ == "__main__":
-    main()
+part2()
